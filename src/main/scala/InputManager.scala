18a2d287cafa351466f0ab871f4a59ac3050540b
Mathis Randl <mathis.randl@epfl.ch>
2020-08-13 10:54:30 +0200

    nothing of interest

--- src/main/scala/InputManager.scala
+++ src/main/scala/InputManager.scala
@@ -7,7 +7,7 @@ import scala.collection.immutable.HashSet
  * When the parser class is published for scala 3, I'll make a cleaner one
  * */
 object InputManager {
-  def noSanitizeInput(str: Iterable[String]) =
+  def noSanitizeInputAndRun(str: Iterable[String]) =
     val strings = str.filterNot(_.startsWith("c")).tail //ignore the first element and comments
     val clauses = strings.map(str => HashSet from (str.dropRight(1) split (" ") filterNot (_.isEmpty) map (_.toLong)) )
     DpllSatSolver.solve(HashSet.from(clauses))
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,7 +7,7 @@ object Main {
   def main(args: Array[String]): Unit = {
     val inputSeq = fromFile(if(args.size == 0) then "res/2-easy.dimacs" else args(0)).getLines().toSeq
     val a = System.currentTimeMillis()
-    val res = (InputManager.noSanitizeInput(inputSeq))
+    val res = InputManager.noSanitizeInputAndRun(inputSeq)
     println("Solving took : " + (System.currentTimeMillis() - a) + "ms")
     res match {
       case None => println("The given formula is unsatisfiable")
