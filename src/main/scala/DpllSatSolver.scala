653468935d09fd22a639e823b79e649976a07b37
Mathis Randl <mathis.randl@epfl.ch>
2020-08-13 17:07:44 +0200

    added comments / small reformattings

--- src/main/scala/DpllSatSolver.scala
+++ src/main/scala/DpllSatSolver.scala
@@ -21,72 +21,93 @@ import Utils._
  **/
 object DpllSatSolver {
 
-  type Literal    = Long // define -x == Not(x), 0 is reserved
+  type Literal    = Long // define -x == Not(x), 0 and Long.MinValue are reserved
   type Assignment = Map[Literal, Boolean]
   type Clause     = Set[Literal]
   type Formula    = Set[Clause] //formula is assumed to be in cnf format
 
   /**
-   * @return all of the literals of the formula
+   * @param formula some Formula
+   * @return all of the literals of the argument
    **/
   private def literalsOf(formula: Formula): Set[Literal] =
     formula.flatten
 
   /**
-   * @return all of the literals, but without negations
+   * @param formula some Formula
+   * @return all of the literals of the argument, but without negations
    **/
   private def absoluteLiteralsOf(formula: Formula): Set[Literal] =
-    literalsOf(formula) map (x => if (x < 0) then -x else x)
+    literalsOf(formula).map(x => if (x < 0) then -x else x)
 
   /**
-   * @return all the pure (that is, always appearing with the same polarity) literals
+   * @param formula some Formula
+   * @return all the pure literals (that is, always appearing with the same polarity) of the argument
    **/
   private def pureLiteralsOf(formula: Formula): Set[Literal] =
     val lits = literalsOf(formula)
-    lits filterNot (literal => lits contains -literal)
+    lits.filterNot(literal => lits contains -literal)
 
   /**
-   * @return all literals that make up a clause all by themselves
+   * @param formula some Formula
+   * @return all literals that make up a clause all by themselves in the argument
    **/
   private def unitLiteralsOf(formula: Formula): Set[Literal] =
-    (formula filter (x => x.size == 1)).flatten 
-
-  private def assignPolarity(formula: Formula, literal: Literal, value: Boolean): Formula =
-    val adjustedLiteral: Literal = if (value) then -literal else literal
-    formula filterNot (_.contains(-adjustedLiteral)) map (x => x - adjustedLiteral)
+    formula.filter(x => x.size == 1).flatten 
+  
+  /**
+   * Assigns true to a literal within a formula and false to its opposite.
+   * This takes polarity into account : for example, assigning true to -2 also assigns false to 2.
+   * 
+   * The resulting formula is guaranteed not to contain the variable anymore.
+   * 
+   * Assigning a value to a variable that does not exist in a formula yields a new formula that deeply equals
+   * the former and may or may not be referentially equal to it, depending on the Set implementation you use.
+   * 
+   * @param formula some Formula containing the variable to assign
+   * @param literal some Literal that will be assigned 
+   * @return a Formula that corresponds to the assignment 
+   * */
+  private def assignTrue(formula: Formula, literal: Literal): Formula =
+    formula.filterNot(_.contains(literal)).map(x => x - (-literal))
     //get rid of the clauses that contain the literal (they're satisfied) and delete the opposite literals
   
+  /**
+   * Returns some absolute literal within the formula.
+   * @param formula some Formula
+   * @return some literal guaranteed to be an absolute literal of the argument
+   */
   private def pickLiteral(formula: Formula): Literal =
     absoluteLiteralsOf(formula).head //todo: room for improvement.
 
   private def checkIfDone(formula: Formula): Option[Boolean] =
     if (formula.isEmpty)
       Some(true) //satisfiable with the given assignment
-    else if (formula exists (_.isEmpty))
+    else if (formula.exists(_.isEmpty))
       Some(false) //unsatisfiable
     else
       None //god only knows, we need to keep working
 
   def solve(formula: Formula): Option[Assignment] =
-    def run(formula: Formula, assignment: Assignment, threadNum: Int): Option[Assignment] =
+    def run(formula: Formula, assignment: Assignment): Option[Assignment] =
       val unitLiterals = unitLiteralsOf(formula)
-      val united = unitLiterals.foldLeft(formula)((f, l) => assignPolarity(f, l, true))
+      val united = unitLiterals.foldLeft(formula)((f, l) => assignTrue(f, l))
       val newAssignment = assignment ++ unitLiterals.map(l => if (l < 0) (-l, false) else (l, true))
 
       checkIfDone(united) match {
         case Some(true) => Some(newAssignment)
         case Some(false) => None
         case None =>
-          val currHead = pickLiteral(united)
-          val remote =  task(run(assignPolarity(united, currHead, true),  newAssignment + ((currHead, true)),  threadNum))
-          val local  =       run(assignPolarity(united, currHead, false), newAssignment + ((currHead, false)), threadNum)
+          val litH = pickLiteral(united)
+          val remote = task(run(assignTrue(united,  litH), newAssignment + ((litH, true))))
+          val local  =      run(assignTrue(united, -litH), newAssignment + ((litH, false)))
           local orElse remote.join //local is guaranteed to be done, so we check it first instead of blocking for remote
       }
 
-    assert(!literalsOf(formula).contains(0), "Sat formula may not contain zero")
+    assert(!literalsOf(formula).exists(x => x == 0 || x == Long.MinValue), "Sat formula may not contain illegal longs.")
     val pureLits = pureLiteralsOf(formula)
-    val purified = pureLits.foldLeft(formula)((f, l) => assignPolarity(f, l, true))
+    val purified = pureLits.foldLeft(formula)((f, l) => assignTrue(f, l))
     val firstAssignment = HashMap.from(pureLits.map(l => if (l < 0) (-l, false) else (l, true)))
-    run(purified, firstAssignment, 1) map 
+    run(purified, firstAssignment) map 
       (assigned => absoluteLiteralsOf(formula).foldLeft(assigned)((opa, l) => if (opa.isDefinedAt(l)) opa else opa + ((l, true))))
 }
